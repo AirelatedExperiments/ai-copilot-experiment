d91bb2037203e421a72146aac6a260a73ab31037
bparida99 <biswojitp11@gmail.com>
2024-03-20 04:24:39 +0530

    first commit

---
+++ src/main/java/aicopilotexperiment/aicopilotexperiment/AiCopilotExperimentApplication.java
@@ -0,0 +1,13 @@
+package aicopilotexperiment.aicopilotexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCopilotExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCopilotExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aicopilotexperiment/aicopilotexperiment/AiCopilotExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package aicopilotexperiment.aicopilotexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCopilotExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
